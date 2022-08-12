0f1c250462963a38fbf6bd5cae830561c6a292a2
zseever <zseever@gmail.com>
2022-08-12 14:33:32 -0700

    set up base html and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
+from ctypes import addressof
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField(max_length=250)
+    phone = models.CharField(max_length=20)
+    cuisine = models.CharField(max_length=20)
+    desc = models.TextField(max_length=1000)
+    img = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+    
