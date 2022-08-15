cc7d32114ac825716e9dc33c8c117188a6a9605a
username <bevcook126@gmail.com>
2022-08-15 16:44:57 -0700

    full CR

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Instruction
+from .models import Instruction, Ingredient
 
 class InstructionForm(ModelForm):
   class Meta:
     model = Instruction
     fields = ['order', 'step']
+
+class IngredientForm(ModelForm):
+  class Meta:
+    model = Ingredient
+    fields = ['quantity', 'name']
--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipes_detail'),
     path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
     path('recipes/<int:recipe_id>/add_instruction/', views.add_instruction, name='add_instructions'),
+    path('recipes/<int:recipe_id>/add_ingredient/', views.add_ingredient, name='add_ingredients'),
 ]
